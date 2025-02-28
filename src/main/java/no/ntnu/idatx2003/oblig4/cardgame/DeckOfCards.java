c715d22276b3f8a5d17afb2a256c8f1e4a48f89f
arpit <arpitsa@stud.ntnu.no>
2025-02-28 16:34:21 +0100

    DeckOfCards
    Created a constructor.
    Created an addToDeck method.
    Created a getCards method.

--- src/main/java/no/ntnu/idatx2003/oblig4/cardgame/DeckOfCards.java
+++ src/main/java/no/ntnu/idatx2003/oblig4/cardgame/DeckOfCards.java
@@ -1,4 +1,33 @@
 package no.ntnu.idatx2003.oblig4.cardgame;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Represents a Deck, which is a collection of {@link PlayingCard} objects.
+ *
+ */
 public class DeckOfCards {
+  private List<PlayingCard> cards;
+  private PlayingCard newPlayingCard;
+  private final char[] suits = { 'S', 'H', 'D', 'C' };
+
+  public DeckOfCards() {
+    cards = new ArrayList<>();
+    addToDeck();
+  }
+
+  public void addToDeck() {
+    for (char suit: suits) {
+      for (int cardsNumber = 1; cardsNumber < 14; cardsNumber++) {
+        newPlayingCard = new PlayingCard(suit, cardsNumber);
+        cards.add(newPlayingCard);
+      }
+    }
+  }
+
+  public List<PlayingCard> getCards() {
+    return cards;
+  }
+
 }
