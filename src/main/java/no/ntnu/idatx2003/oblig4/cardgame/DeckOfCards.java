4e15f96988d8e433b6e286bcd4ab11e9235dbf37
arpit <arpitsa@stud.ntnu.no>
2025-03-03 12:59:51 +0100

    DeckOfCards
    Created dealHand method

--- src/main/java/no/ntnu/idatx2003/oblig4/cardgame/DeckOfCards.java
+++ src/main/java/no/ntnu/idatx2003/oblig4/cardgame/DeckOfCards.java
@@ -2,19 +2,23 @@ package no.ntnu.idatx2003.oblig4.cardgame;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 /**
  * Represents a Deck, which is a collection of {@link PlayingCard} objects.
  *
+ * @version 0.1.0
  */
 public class DeckOfCards {
   private List<PlayingCard> cards;
   private PlayingCard newPlayingCard;
   private final char[] suits = { 'S', 'H', 'D', 'C' };
+  private Random randomCard;
 
   public DeckOfCards() {
     cards = new ArrayList<>();
     addToDeck();
+    randomCard = new Random();
   }
 
   public void addToDeck() {
@@ -30,4 +34,12 @@ public class DeckOfCards {
     return cards;
   }
 
+  public List<PlayingCard> dealHand(int n) {
+    List<PlayingCard> hand = new ArrayList<>();
+    for (int i = 0; i < n; i++) {
+      int index = randomCard.nextInt(cards.size());
+      hand.add(cards.get(index));
+    }
+    return hand;
+  }
 }
