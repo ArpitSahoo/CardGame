a33645a8269d78f4cb5e3d7c5877d8e79a65161f
arpit <arpitsa@stud.ntnu.no>
2025-03-03 17:29:55 +0100

    Hand
    Created dealToHand and getHandSize method.

--- src/main/java/no/ntnu/idatx2003/oblig4/cardgame/Hand.java
+++ src/main/java/no/ntnu/idatx2003/oblig4/cardgame/Hand.java
@@ -1,13 +1,11 @@
 package no.ntnu.idatx2003.oblig4.cardgame;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Random;
 
 /**
  * Represents the dealers hand.
  *
- * @version 0.0.1
+ * @version 0.1.0
  */
 public class Hand {
   private DeckOfCards deck;
@@ -19,22 +17,34 @@ public class Hand {
   }
 
 
-  public boolean newRound() {
+  public void newRound() {
     boolean newGame = false;
     if (cardsInHand.isEmpty()) {
       newGame = true;
-      addToHand();
+      dealToHand(6); // 6 til the user interface has been developed.
     }
     removeCardsFromHand();
-    return newGame;
   }
 
   public void removeCardsFromHand() {
     cardsInHand.clear();
   }
 
+  public void dealToHand(int n){
+    if (n <= 0) {
+      throw new IllegalArgumentException("Dealer can not deal 0 or less");
+    }
+    for (int i = 0; i < n; i++) {
+      addToHand();
+    }
+  }
+
   public void addToHand() {
     cardsInHand.add(deck.getRandomCard());
+  }
+
+  public int getHandSize() {
+    return cardsInHand.size();
   }
 
 
