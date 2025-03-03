68c9b793724e57a92c6b4b1c8a433f1022faddf6
arpit <arpitsa@stud.ntnu.no>
2025-03-03 16:11:01 +0100

    DeckOfCards
    Created getSize, getRandomCard, randomIndexNumber, getRandomCard and removeCardFromDeck

--- src/main/java/no/ntnu/idatx2003/oblig4/cardgame/DeckOfCards.java
+++ src/main/java/no/ntnu/idatx2003/oblig4/cardgame/DeckOfCards.java
@@ -1,6 +1,7 @@
 package no.ntnu.idatx2003.oblig4.cardgame;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Random;
 
@@ -13,14 +14,22 @@ public class DeckOfCards {
   private List<PlayingCard> cards;
   private PlayingCard newPlayingCard;
   private final char[] suits = { 'S', 'H', 'D', 'C' };
-  private Random randomCard;
+  private final Random randomCard;
 
+  /**
+   * Creates a deck of cards.
+   *
+   */
   public DeckOfCards() {
     cards = new ArrayList<>();
     addToDeck();
     randomCard = new Random();
   }
 
+  /**
+   * A method that creates and adds cards to the deck.
+   *
+   */
   public void addToDeck() {
     for (char suit: suits) {
       for (int cardsNumber = 1; cardsNumber < 14; cardsNumber++) {
@@ -30,16 +39,32 @@ public class DeckOfCards {
     }
   }
 
+  /**
+   * A method that retrieves cards from the deck.
+   *<p>This method is used for the test class.</p>
+   * @return cards.
+   */
   public List<PlayingCard> getCards() {
-    return cards;
+    return cards; //only for test methods.
   }
 
-  public List<PlayingCard> dealHand(int n) {
-    List<PlayingCard> hand = new ArrayList<>();
-    for (int i = 0; i < n; i++) {
-      int index = randomCard.nextInt(cards.size());
-      hand.add(cards.get(index));
-    }
-    return hand;
+  public int getSize(){
+    return cards.size();
   }
+
+  public int randomIndexNumber() {
+    return randomCard.nextInt(getSize());
+  }
+
+  public PlayingCard getRandomCard() {
+    int index = randomIndexNumber(); // Get a random index
+    PlayingCard randomCard = cards.get(index); // Retrieve the card at that index
+    removeCardFromDeck(index);
+    return randomCard; // Return the random card
+  }
+
+  public void removeCardFromDeck(int index) {
+    cards.remove(index);
+  }
+
 }
