2ab1a99005210ada02cde6a1a38e6d3c85cb9deb
arpit <arpitsa@stud.ntnu.no>
2025-03-08 18:55:31 +0100

    Updated the constructor to be more functional.
    getCardsToView got a new method in it.
    
    Created a new method called checkAllCards.

--- src/main/java/no/ntnu/idatx2003/oblig4/cardgame/controller/SystemInterface.java
+++ src/main/java/no/ntnu/idatx2003/oblig4/cardgame/controller/SystemInterface.java
@@ -1,22 +1,26 @@
 package no.ntnu.idatx2003.oblig4.cardgame.controller;
-
-import no.ntnu.idatx2003.oblig4.cardgame.dealer.Hand;
 import no.ntnu.idatx2003.oblig4.cardgame.deck.DeckOfCards;
 import no.ntnu.idatx2003.oblig4.cardgame.gui.View;
 
 public class SystemInterface {
   private DeckOfCards deck;
   private View view;
-  private Hand hand;
 
   public SystemInterface(DeckOfCards deck, View view) {
-    deck = new DeckOfCards();
-    view = new View();
+    this.deck = deck;
+    this.view = view;
   }
 
   public void getCardsToView() {
     deck.dealToHand();
-
+    view.updateCardsInHand();
   }
 
+  public void checkAllCards() {
+    deck.isFlush();
+    deck.isHearts();
+    deck.isSpareQueen();
+    deck.sumOfCards();
+    view.updateAllChecks();
+  }
 }
