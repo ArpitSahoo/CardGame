a0cbb023cebc66acb7bc333db0fdb3f777f15276
arpit <arpitsa@stud.ntnu.no>
2025-03-08 18:56:50 +0100

    Updated the constructor to be more functional.
    getCardsToView got a new method in it.
    
    Created a new method called checkAllCards.

--- src/main/java/no/ntnu/idatx2003/oblig4/cardgame/controller/SystemInterface.java
+++ src/main/java/no/ntnu/idatx2003/oblig4/cardgame/controller/SystemInterface.java
@@ -2,6 +2,13 @@ package no.ntnu.idatx2003.oblig4.cardgame.controller;
 import no.ntnu.idatx2003.oblig4.cardgame.deck.DeckOfCards;
 import no.ntnu.idatx2003.oblig4.cardgame.gui.View;
 
+/**This class has the responsibility to create a communication
+ * between the logical og the GUI in the system.
+ *
+ * @author Arpit
+ * @version 0.2.0
+ */
+
 public class SystemInterface {
   private DeckOfCards deck;
   private View view;
