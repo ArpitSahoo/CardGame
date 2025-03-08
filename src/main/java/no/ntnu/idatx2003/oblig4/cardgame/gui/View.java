dc839aff0bd0bfbec5c29f7ec6ff4c67b81b921b
arpit <arpitsa@stud.ntnu.no>
2025-03-08 18:57:23 +0100

    Major changes in the code.
    new formats, new methods and a connection between GUI and the system.

--- src/main/java/no/ntnu/idatx2003/oblig4/cardgame/gui/View.java
+++ src/main/java/no/ntnu/idatx2003/oblig4/cardgame/gui/View.java
@@ -5,7 +5,9 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.*;
+import javafx.scene.text.Font;
 import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 import no.ntnu.idatx2003.oblig4.cardgame.controller.SystemInterface;
 import no.ntnu.idatx2003.oblig4.cardgame.deck.DeckOfCards;
@@ -16,16 +18,20 @@ import no.ntnu.idatx2003.oblig4.cardgame.deck.DeckOfCards;
  *
  * @since 0.0.1
  * @author arpit
- * @version 0.0.1
+ * @version 0.2.0
  */
 public class View extends Application {
   private SystemInterface controller;
-  DeckOfCards deck;
-
-
+  private DeckOfCards deck;
+  private Text cardsInHand;
+  private Text heartsFound;
+  private Text wasFound;
+  private Text flushFound;
+  private Text totalSum;
 
   @Override
   public void start(Stage stage) throws Exception {
+    deck = new DeckOfCards();
     controller = new SystemInterface(deck, this);
     BorderPane borderPane = new BorderPane();
     borderPane.setCenter(centerPaneCreate());
@@ -37,38 +43,56 @@ public class View extends Application {
     stage.show();
   }
 
-
   public Pane centerPaneCreate() {
     VBox centerPane = new VBox();
-    FlowPane buttonPane = getButtons();
-    HBox cardPane = getCardPane();
-    GridPane checkCardPane = getCheckCardPane();
-
-    centerPane.getChildren().addAll(cardPane, buttonPane, checkCardPane);
+    centerPane.getChildren().addAll(buttonAndCards(), getCheckCardPane());
     return centerPane;
   }
 
-  private static HBox getCardPane() {
-    HBox cardPane = new HBox();
-    Text card = new Text(" ");
+  public HBox buttonAndCards() {
+    HBox buttonAndCards = new HBox();
+    buttonAndCards.getChildren().addAll(getCardPane(), getButtons());
+    return buttonAndCards;
+  }
 
-    cardPane.setSpacing(10);
-    cardPane.getChildren().addAll(card);
+  public FlowPane getCardPane() {
+    FlowPane cardPane = new FlowPane();
+    cardPane.setStyle("-fx-background-color: lightblue;");
+    cardsInHand = new Text("");
+    cardsInHand.setFont(new Font(40));
+    cardsInHand.setTextAlignment(TextAlignment.CENTER);
+    cardPane.getChildren().add(cardsInHand);
     return cardPane;
   }
 
-  private static FlowPane getButtons() {
+  public void updateCardsInHand() {
+    if (deck != null && cardsInHand != null) {
+      cardsInHand.setText(deck.getCardsFromHand());
+    }
+  }
+
+  private FlowPane getButtons() {
     FlowPane buttonPane = new FlowPane();
 
     Button add1button = new Button("Deal hand");
+    add1button.setPrefWidth(200);
+    add1button.setPrefHeight(30);
+    add1button.setOnAction(event ->
+      controller.getCardsToView()
+    );
+
     Button add2button = new Button("Check hand");
+    add2button.setPrefWidth(200);
+    add2button.setPrefHeight(30);
+    add2button.setOnAction(event ->
+        controller.checkAllCards());
 
     buttonPane.getChildren().addAll(add1button, add2button);
-    buttonPane.setAlignment(Pos.BASELINE_RIGHT);
+    buttonPane.setAlignment(Pos.TOP_RIGHT);
     return buttonPane;
   }
 
-  public static GridPane getCheckCardPane() {
+  public GridPane getCheckCardPane() {
     GridPane checkCardPane = new GridPane();
 
     checkCardPane.add(getHeartsPane(), 0, 0);
@@ -81,20 +105,20 @@ public class View extends Application {
     return checkCardPane;
   }
 
-  public static GridPane getCheckSum() {
+  public GridPane getCheckSum() {
     GridPane checkSumPane = new GridPane();
     Text sum = new Text("Sum: ");
-    Text totalSum = new Text("0");
+    totalSum = new Text("0");
     checkSumPane.add(sum, 1, 0);
-    checkSumPane.add(totalSum, 1, 1);
+    checkSumPane.add(totalSum, 2, 0);
     checkSumPane.setVgap(10);
     return checkSumPane;
   }
 
-  public static GridPane getHeartsPane() {
+  public GridPane getHeartsPane() {
     GridPane heartsPane = new GridPane();
     Text hearts = new Text("Hearts: ");
-    Text heartsFound = new Text("Something");
+    heartsFound = new Text("Something");
 
     heartsPane.add(hearts, 0, 0);
     heartsPane.add(heartsFound, 1, 0);
@@ -102,10 +126,10 @@ public class View extends Application {
     return heartsPane;
   }
 
-  public static GridPane getSpareQueenPane() {
+  public GridPane getSpareQueenPane() {
     GridPane spareQueenPane = new GridPane();
-    Text spareQueen = new Text("Spare Queen ");
-    Text wasFound = new Text("yes/no");
+    Text spareQueen = new Text("Spare Queen: ");
+    wasFound = new Text("yes/no");
 
     spareQueenPane.add(spareQueen, 0, 0);
     spareQueenPane.add(wasFound, 1, 0);
@@ -113,16 +137,46 @@ public class View extends Application {
     return spareQueenPane;
   }
 
-  public static GridPane getFlushPane() {
+  public GridPane getFlushPane() {
     GridPane flushPane = new GridPane();
     Text flush = new Text("Flush: ");
-    Text flushFound = new Text("yes/no");
+    flushFound = new Text("yes/no");
     flushPane.add(flush, 0, 0);
     flushPane.add(flushFound, 1, 0);
     flushPane.setVgap(10);
     return flushPane;
   }
 
+  public void updateAllChecks() {
+    updateSum();
+    updateCheckForHearts();
+    updateCheckForSpareQueen();
+    updateCheckForFlush();
+  }
+
+  private void updateSum() {
+    if (deck != null && cardsInHand != null) {
+      totalSum.setText(Integer.toString(deck.sumOfCards()));
+    }
+  }
+
+  public void updateCheckForHearts() {
+    if (deck != null && cardsInHand != null) {
+      heartsFound.setText(deck.isHearts());
+    }
+  }
+
+  public void updateCheckForSpareQueen() {
+    if (deck != null && cardsInHand != null) {
+      wasFound.setText(deck.isSpareQueen());
+    }
+  }
+
+  public void updateCheckForFlush() {
+    if (deck != null && cardsInHand != null) {
+      flushFound.setText(deck.isFlush());
+    }
+  }
 
   public static void main(String[] args) {
     launch(args);
