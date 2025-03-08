c5a1c769a7f83527fdd8934bad9b47d5a84761bd
arpit <arpitsa@stud.ntnu.no>
2025-03-08 18:54:43 +0100

    Removed try catch.

--- src/main/java/no/ntnu/idatx2003/oblig4/cardgame/Main.java
+++ src/main/java/no/ntnu/idatx2003/oblig4/cardgame/Main.java
@@ -5,12 +5,6 @@ import no.ntnu.idatx2003.oblig4.cardgame.gui.View;
 public class Main {
 
   public static void main(String[] args) {
-    try {
-      View.main(args);
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
-
-
+    View.main(args);
   }
 }
