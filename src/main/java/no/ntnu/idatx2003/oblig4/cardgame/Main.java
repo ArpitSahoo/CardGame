76a09b506c5e4ad466546c2377965930c88d2035
arpit <arpitsa@stud.ntnu.no>
2025-03-12 08:45:03 +0100

    Created documentation and added a try catch exception

--- src/main/java/no/ntnu/idatx2003/oblig4/cardgame/Main.java
+++ src/main/java/no/ntnu/idatx2003/oblig4/cardgame/Main.java
@@ -1,10 +1,26 @@
 package no.ntnu.idatx2003.oblig4.cardgame;
 
 import no.ntnu.idatx2003.oblig4.cardgame.gui.View;
-
+/**
+ * The Main class starts the application by creating and displaying the GUI.
+ * It invokes the View class, which represents the graphical user interface.
+ *
+ * @since 0.0.1
+ * @author arpit
+ * @version 0.1.0
+ */
 public class Main {
 
+  /**
+   * The main method to launch the application.
+   *
+   * @param args Command-line arguments (not used).
+   */
   public static void main(String[] args) {
-    View.main(args);
+    try {
+      View.main(args);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
   }
 }
